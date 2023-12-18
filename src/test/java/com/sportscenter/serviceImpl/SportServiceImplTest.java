c9c14b1c10910d152aa83ccc0a9868c51fd080f4
Anton Goranov <agoranovv@gmail.com>
2023-12-19 00:04:28 +0200

    sportService and userRoleService tests

---
+++ src/test/java/com/sportscenter/serviceImpl/SportServiceImplTest.java
@@ -0,0 +1,51 @@
+package com.sportscenter.serviceImpl;
+
+import com.sportscenter.model.entity.SportEntity;
+import com.sportscenter.model.mapper.SportMapper;
+import com.sportscenter.model.view.SportViewModel;
+import com.sportscenter.repository.SportRepository;
+import com.sportscenter.service.impl.SportServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class SportServiceImplTest {
+
+    @Mock
+    private SportRepository sportRepositoryMock;
+    @Mock
+    private SportMapper sportMapperMock;
+
+    @InjectMocks
+    private SportServiceImpl sportServiceMock;
+
+    @Test
+    public void testGetAllSport_returnsAList(){
+        List<SportEntity> sportEntityListTest = Collections.singletonList(new SportEntity());
+        when(sportRepositoryMock.findAll())
+                .thenReturn(sportEntityListTest);
+
+        List<SportViewModel> mockSportViewModels = Collections.singletonList(new SportViewModel());
+        when(sportMapperMock.sportEntityToViewModel(any(SportEntity.class)))
+                .thenReturn(mockSportViewModels.get(0));
+
+
+        List<SportViewModel> actual = sportServiceMock.getAllSports();
+
+
+        assertEquals(mockSportViewModels.size(), actual.size());
+        verify(sportMapperMock, times(sportEntityListTest.size()))
+                .sportEntityToViewModel(any(SportEntity.class));
+    }
+
+}
---
+++ src/test/java/com/sportscenter/serviceImpl/UserRoleServiceImplTest.java
@@ -0,0 +1,58 @@
+package com.sportscenter.serviceImpl;
+
+import com.sportscenter.model.entity.SportEntity;
+import com.sportscenter.model.entity.UserRoleEntity;
+import com.sportscenter.model.mapper.UserRoleMapper;
+import com.sportscenter.model.view.SportViewModel;
+import com.sportscenter.model.view.UserRoleViewModel;
+import com.sportscenter.repository.UserRoleRepository;
+import com.sportscenter.service.impl.UserRoleServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class UserRoleServiceImplTest {
+
+    @Mock
+    private UserRoleRepository userRoleRepository;
+    @Mock
+    private UserRoleMapper userRoleMapper;
+
+    @InjectMocks
+    private UserRoleServiceImpl userRoleServiceMock;
+
+    @Test
+    void testFindAll() {
+
+        List<UserRoleEntity> mockUserRoles = List.of(new UserRoleEntity());
+        when(userRoleRepository.findAll())
+                .thenReturn(mockUserRoles);
+
+        Set<UserRoleViewModel> mockUserRoleViewModels = Set.of(new UserRoleViewModel());
+        when(userRoleMapper.userRoleEntityToViewModel(any(UserRoleEntity.class)))
+                .thenReturn(mockUserRoleViewModels.iterator().next());
+
+
+        Set<UserRoleViewModel> result = userRoleServiceMock.findAll();
+
+
+        assertEquals(mockUserRoles.size(), result.size());
+        verify(userRoleMapper, times(mockUserRoles.size())).userRoleEntityToViewModel(any());
+    }
+
+}
